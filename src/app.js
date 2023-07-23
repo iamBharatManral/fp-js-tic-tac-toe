5acbf2dd28e351b3f192ce35295ccb504842df94
iamBharatManral <justbharatmanral@gmail.com>
2023-07-23 17:40:52 +0530

    refactor: change the winning log to simpler one using predefined winning scenarios

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import {all, always, any, cond, equals, ifElse, map, pipe} from "ramda";
-import { Fireworks } from 'fireworks-js'
+import {Fireworks} from 'fireworks-js'
 
 const players = {
     Player1: Symbol(1),
     Player2: Symbol(2)
 }
 
-const winningCombos = [[1, 2, 3], [4, 5, 6], [7,8,9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+const winningCombos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
 const playerMarks = {
     [players.Player1]: () => "X",
@@ -75,85 +75,58 @@ const outcomes = {
 
 const getPlayerMark = player => playerMarks[player]()
 
-const isLeftPlayerSame = ({currentCell: cell, board, currentPlayer: player}) => {
-    return [3, 6, 9].includes(cell)
-        ? board[cell-1]["player"] === player && board[cell-2]["player"] === player
-        : [2, 5, 8].includes(cell)
-            ? board[cell-1]["player"] === player
-            : true
-}
-
-const isRightPlayerSame = ({currentCell: cell, board, currentPlayer: player}) => {
-    return [1, 4, 7].includes(cell)
-        ? board[cell+1]["player"] === player && board[cell+2]["player"] === player
-        : [2, 5, 8].includes(cell)
-            ? board[cell+1]["player"] === player
-            : true
-}
-
-const isUpPlayerSame = ({currentCell: cell, board, currentPlayer: player}) => {
-    return [7, 8, 9].includes(cell)
-        ? board[cell-3]["player"] === player && board[cell-6]["player"] === player
-        : [4, 5, 6].includes(cell)
-            ? board[cell-3]["player"] === player
-            : true
-}
-
-const isDownPlayerSame = ({currentCell: cell, board, currentPlayer: player}) => {
-    return [1, 2, 3].includes(cell)
-        ? board[cell+3]["player"] === player && board[cell+6]["player"] === player
-        : [4, 5, 6].includes(cell)
-            ? board[cell+3]["player"] === player
-            : true
-}
-
-const checkRow = state => all(equals(true), [isLeftPlayerSame(state), isRightPlayerSame(state)])
-
-const checkCol = state => all(equals(true), [isUpPlayerSame(state), isDownPlayerSame(state)])
-
-const checkDiagonal = state => {
-    const {board, currentPlayer: player, currentCell: cell} = state
-    return cond([
-        [(b, cell) => [2, 4, 6, 8].includes(cell), always(false)],
-        [(b, cell) => cell === 1, (board, cell, player) => (board[cell + 4]["player"] === player && board[cell + 8]["player"] === player)],
-        [(b, cell) => cell === 3, (board, cell, player) => (board[cell + 2]["player"] === player && board[cell + 4]["player"] === player)],
-        [(b, cell) => cell === 5, (board, cell, player) => (board[cell - 2]["player"] === player && board[cell + 2]["player"] === player)
-            || (board[cell - 4]["player"] === player && board[cell + 4]["player"] === player)],
-        [(b, cell) => cell === 7, (board, cell, player) => (board[cell - 2]["player"] === player && board[cell - 4]["player"] === player)],
-        [(b, cell) => cell === 9, (board, cell, player) => (board[cell - 4]["player"] === player && board[cell - 8]["player"] === player)
-        ],
-    ])(board, cell, player)
-}
-
 const isBoardFull = state => all(equals(true), Object.values(map(cell => !!cell.player, state.board)))
 
-const continueOrDrawGame = state => isBoardFull(state) ? {outcome: outcomes.draw, state} : {outcome: outcomes.continue, state}
+const continueOrDrawGame = state => isBoardFull(state) ? {outcome: outcomes.draw, state} : {
+    outcome: outcomes.continue,
+    state
+}
 
 const finishGame = (state) => ({outcome: outcomes.win, state})
 
 const getCurrentPlayer = lastPlayer => cond([
-        [equals(null), always(players.Player1)],
-        [equals(players.Player1), always(players.Player2)],
-        [equals(players.Player2), always(players.Player1)]
-        ])(lastPlayer)
+    [equals(null), always(players.Player1)],
+    [equals(players.Player1), always(players.Player2)],
+    [equals(players.Player2), always(players.Player1)]
+])(lastPlayer)
 
-
-const haveWon = (state) => any(equals(true), [checkRow(state), checkCol(state), checkDiagonal(state)])
+const getWinningCombo = state => {
+    let winningCells = []
+    const player1Cells = Object.keys(state.board).filter(
+        key => state["board"][key]["player"] === players.Player1
+    ).sort()
+    const player2Cells = Object.keys(state.board).filter(
+        key => state["board"][key]["player"] === players.Player2
+    ).sort()
+    winningCombos.forEach(combo => {
+            if (combo.every(item => player1Cells.includes(item.toString()))) {
+                winningCells = combo
+            } else if (combo.every(item => player2Cells.includes(item.toString()))) {
+                winningCells = combo
+            }
+        }
+    )
+    return winningCells
+}
+const checkWinningCombo = state => {
+    return getWinningCombo(state).length !== 0
+}
+const haveWon = (state) => any(equals(true), [checkWinningCombo(state)])
 
 const decideOutcome = ifElse(haveWon, finishGame, continueOrDrawGame)
 
 const updateBoard = (state, cell) => {
     const {board, currentPlayer: player} = state;
     const currentPlayer = getCurrentPlayer(player)
-    if(board[cell]["player"] !== null){
+    if (board[cell]["player"] !== null) {
         return state
     }
     return {
         board: {
-        ...board,
-                [cell]: {
-            ...board[cell],
-                    player: !board[cell][player] ? currentPlayer: board[cell][player]
+            ...board,
+            [cell]: {
+                ...board[cell],
+                player: !board[cell][player] ? currentPlayer : board[cell][player]
             }
         },
         currentCell: parseInt(cell),
@@ -164,9 +137,8 @@ const updateBoard = (state, cell) => {
 const playGame = pipe(updateBoard, decideOutcome)
 
 const app = () => {
-    const board = document.querySelector(".board")
     const firework = document.querySelector(".firework")
-    const fireworks = new Fireworks(firework, { /* options */ })
+    const fireworks = new Fireworks(firework, { /* options */})
     const newBtn = document.querySelector(".new")
     const cells = document.getElementsByClassName("cell")
     const message = document.querySelector(".message")
@@ -181,73 +153,58 @@ const app = () => {
     })
 
     const markWinOnUI = () => {
-        let winningCells;
-        const player1Cells = Object.keys(state.board).filter(
-            key => state["board"][key]["player"] === players.Player1
-            ).sort()
-        const player2Cells = Object.keys(state.board).filter(
-            key => state["board"][key]["player"] === players.Player2
-        ).sort()
-        winningCombos.forEach(combo => {
-                if (combo.every(item => player1Cells.includes(item.toString()))) {
-                    winningCells = combo
-                } else if (combo.every(item => player2Cells.includes(item.toString()))) {
-                    winningCells = combo
-                }
-            }
-        )
+        const winningCells = getWinningCombo(state);
 
         winningCells.forEach(cell => {
             console.log(cell)
-            cells[cell-1].style.textDecoration = "line-through"
+            cells[cell - 1].style.textDecoration = "line-through"
         })
     }
 
-    const freezeBoard = () => Array.from(cells).forEach(cell =>{
+    const freezeBoard = () => Array.from(cells).forEach(cell => {
         cell.style.pointerEvents = "none"
     })
 
-    const unfreezeBoard = () => Array.from(cells).forEach(cell =>{
+    const unfreezeBoard = () => Array.from(cells).forEach(cell => {
         cell.style.pointerEvents = "auto"
     })
-
 
     const clearMessage = () => message.innerText = ""
 
     const setMessage = (gameState, playerMark) => {
-        if(gameState === "win"){
+        if (gameState === "win") {
             message.innerText = playerMark === "X" ? "Whoopie! Player 1 has Won!" : "Whoopie! Player 2 has Won!"
             message.style.color = "darkgreen"
-        }else{
+        } else {
             message.innerText = "Oops! This game is a Draw!"
             message.style.color = "red"
         }
     }
     const markPlayerOnUI = () => {
-        const element = cells[state.currentCell-1]
+        const element = cells[state.currentCell - 1]
         const playerMark = getPlayerMark(state.currentPlayer)
         element.innerText = playerMark
         element.style.color = playerMark === "X" ? "whitesmoke" : "yellow"
     }
 
     const setPlayerTurn = playerMark => {
-        const player = playerMark  === "X" ? "O" : "X"
+        const player = playerMark === "X" ? "O" : "X"
         playerTurn.innerText = `It's ${player}'s turn!`
     }
-    const clearPlayerTurn = () =>{
+    const clearPlayerTurn = () => {
         playerTurn.innerText = ""
     }
     const setPressedEffect = (cell) => {
-        cells[cell-1].style.scale = "0.98";
-        cells[cell-1].style.boxShadow = "0 0 3px 3px lightgray"
+        cells[cell - 1].style.scale = "0.98";
+        cells[cell - 1].style.boxShadow = "0 0 3px 3px lightgray"
     }
     const handleInput = (event) => {
         const cellNumber = event.target.classList[1].split("-")[1]
         setPressedEffect(cellNumber)
-        const {outcome, state: newState } = playGame(state, cellNumber)
+        const {outcome, state: newState} = playGame(state, cellNumber)
         state = newState
         setPlayerTurn(getPlayerMark(state.currentPlayer))
-        if(outcome === outcomes.win){
+        if (outcome === outcomes.win) {
             clearPlayerTurn()
             markPlayerOnUI()
             markWinOnUI()
@@ -258,12 +215,12 @@ const app = () => {
                 fireworks.pause()
                 fireworks.clear()
             }, 5000)
-        }else if(outcome=== outcomes.draw){
+        } else if (outcome === outcomes.draw) {
             clearPlayerTurn()
             markPlayerOnUI()
             setMessage("draw", getPlayerMark(state.currentPlayer))
             freezeBoard()
-        }else{
+        } else {
             setPlayerTurn(getPlayerMark(state.currentPlayer))
             markPlayerOnUI()
         }
